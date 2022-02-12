b8a7beb4bff46911830e898a68adc603f899dde1
Cup-Tess <muteva2005@gmail.com>
2022-02-12 23:33:41 +0300

    'switch_url'

--- money/urls.py
+++ money/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.menu, name='menu'),
+    path('menu/', views.menu, name='menu'),
     path('new/', views.post_new, name='for_forms'),
-    path('auth/', views.auth, name="auth"),
+    path('', views.auth, name="auth"),
     path('signup/', views.SignUp.as_view(), name="signup"),
 ]
