45060f03de04729fc097b5696bddd57424fa47e7
Cup-Tess <muteva2005@gmail.com>
2022-02-12 23:01:37 +0300

    menu create

--- money/views.py
+++ money/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -7,7 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 
 def auth(request):
-    return render(request,"money/auth.html")
+    return render(request, "money/auth.html")
 
 class SignUp(CreateView):
     form_class = UserCreationForm
@@ -15,8 +15,26 @@ class SignUp(CreateView):
     template_name = "registration/signup.html"
 
 def menu(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'money/menu.html', {'posts': posts})
+    count = count_1 = count_2 = count_3 = count_4 = count_5 = count_6 = count_7 = 0
+    posts = Post.objects.filter(author=request.user)
+    for post in posts:
+        count += post.summa
+        if post.selector =='П':
+            count_1 += post.summa
+        elif post.selector == 'Т':
+            count_2 += post.summa
+        elif post.selector == 'О':
+            count_3 += post.summa
+        elif post.selector == 'С':
+            count_4 += post.summa
+        elif post.selector == 'А':
+            count_5 += post.summa
+        elif post.selector == 'Ж':
+            count_6 += post.summa
+        elif post.selector == 'Д':
+            count_7 += post.summa
+    return render(request, 'money/menu.html', {'count_1': count_1, 'count_2': count_2, 'count_3': count_3, 'count_4': count_4, 'count_5': count_5, 'count_6': count_6, 'count_7': count_7, 'count': count, })
+
 
 def post_new(request):
     if request.method == "POST":
@@ -26,6 +44,8 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
+            return redirect('/')
+
     else:
         form = PostForm()
     return render(request, 'money/for_forms.html', {'form': form})
